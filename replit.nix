5b2a37cd41d13204f5440cf2786e8da19989f990
WHIZ MBURU <151882632+Whizmburu@users.noreply.github.com>
2024-05-25 15:53:39 +0300

    Add files via upload

--- replit.nix
+++ replit.nix
@@ -1,22 +1,3 @@
-{ pkgs }: {
-    deps = [ 
-        pkgs.nodejs
-        pkgs.nodePackages.typescript
-        pkgs.ffmpeg
-        pkgs.imagemagick
-        pkgs.git
-        pkgs.neofetch
-        pkgs.libwebp
-        pkgs.speedtest-cli
-        pkgs.wget
-        pkgs.yarn
-        pkgs.libuuid
-    ]; 
-    env = {
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.libuuid
-        ];
-    };
-}  
-            
-              
+{pkgs}: {
+  deps = [ ];
+}
